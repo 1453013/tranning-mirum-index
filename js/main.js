eef6b177ea093342e56a58ad143eeb2efda5ad39
Trà Ngọc Hải <1453013@student.hcmus.edu.vn>
2017-08-03 12:10:23 +0700

    remove files
    remove unnecessary library

--- js/main.js
+++ js/main.js
@@ -15,91 +15,91 @@ jQuery(function(){
     });
     
 
-//config for slick-slider
-$('.portfolio-item-slider').on('init', function(event, slick, currentSlide){
-  var nrCurrentSlide = slick.currentSlide + 1, // din cauza ca e array si incepe de la 0
-      totalSlidesPerPage = nrCurrentSlide + 4; // daca ai 5 thumb-uri pe pagina pui + 4
-  $('.controls').html(nrCurrentSlide + " - " + totalSlidesPerPage + " of " + slick.slideCount);
-});
+// //config for slick-slider
+// $('.portfolio-item-slider').on('init', function(event, slick, currentSlide){
+//   var nrCurrentSlide = slick.currentSlide + 1, // din cauza ca e array si incepe de la 0
+//       totalSlidesPerPage = nrCurrentSlide + 4; // daca ai 5 thumb-uri pe pagina pui + 4
+//   $('.controls').html(nrCurrentSlide + " - " + totalSlidesPerPage + " of " + slick.slideCount);
+// });
 
-$('.portfolio-thumb-slider').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  asNavFor: '.portfolio-item-slider',
-  arrows: false,
-  focusOnSelect: true,
-  infinite: false,
-  //prevArrow: $('.slick-prev'),
-  //nextArrow: $('.next'),
-  prevArrow:"<img class='a-left control-c prev slick-prev' src='../traning/js/left-arrow.png'>",
-  nextArrow:"<img class='a-right control-c next slick-next' src='../traning/js/right-arrow.png'>"
-});
+// $('.portfolio-thumb-slider').slick({
+//   slidesToShow: 5,
+//   slidesToScroll: 1,
+//   asNavFor: '.portfolio-item-slider',
+//   arrows: false,
+//   focusOnSelect: true,
+//   infinite: false,
+//   //prevArrow: $('.slick-prev'),
+//   //nextArrow: $('.next'),
+//   prevArrow:"<img class='a-left control-c prev slick-prev' src='../traning/js/left-arrow.png'>",
+//   nextArrow:"<img class='a-right control-c next slick-next' src='../traning/js/right-arrow.png'>"
+// });
 
-$('.portfolio-item-slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  asNavFor: '.portfolio-thumb-slider',
-  infinite: false,
-  prevArrow:"<img class='a-left control-c prev slick-prev' src='../traning/js/left-arrow.png'>",
-  nextArrow:"<img class='a-right control-c next slick-next' src='../traning/js/right-arrow.png'>"
-});
+// $('.portfolio-item-slider').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: true,
+//   asNavFor: '.portfolio-thumb-slider',
+//   infinite: false,
+//   prevArrow:"<img class='a-left control-c prev slick-prev' src='../traning/js/left-arrow.png'>",
+//   nextArrow:"<img class='a-right control-c next slick-next' src='../traning/js/right-arrow.png'>"
+// });
 
-var current = 0; // current slider dupa refresh
-$('.portfolio-thumb-slider .slick-slide:not(.slick-cloned)').eq(current).addClass('slick-current');
-$('.portfolio-item-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-  current = currentSlide;
-  $('.portfolio-thumb-slider .slick-slide').removeClass('slick-current');
-  $('.portfolio-thumb-slider .slick-slide:not(.slick-cloned)').eq(current).addClass('slick-current');
-  var nrCurrentSlide = slick.currentSlide + 1, // din cauza ca e array si incepe de la 0
-      totalSlidesPerPage = nrCurrentSlide + 4; // daca ai 5 thumb-uri pe pagina pui + 4
+// var current = 0; // current slider dupa refresh
+// $('.portfolio-thumb-slider .slick-slide:not(.slick-cloned)').eq(current).addClass('slick-current');
+// $('.portfolio-item-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+//   current = currentSlide;
+//   $('.portfolio-thumb-slider .slick-slide').removeClass('slick-current');
+//   $('.portfolio-thumb-slider .slick-slide:not(.slick-cloned)').eq(current).addClass('slick-current');
+//   var nrCurrentSlide = slick.currentSlide + 1, // din cauza ca e array si incepe de la 0
+//       totalSlidesPerPage = nrCurrentSlide + 4; // daca ai 5 thumb-uri pe pagina pui + 4
 
-  if(totalSlidesPerPage > slick.slideCount) {
-    $('.controls').html(nrCurrentSlide + " - " + slick.slideCount + " of " + slick.slideCount);
-  } else {
-    $('.controls').html(nrCurrentSlide + " - " + totalSlidesPerPage + " of " + slick.slideCount);
-  }
+//   if(totalSlidesPerPage > slick.slideCount) {
+//     $('.controls').html(nrCurrentSlide + " - " + slick.slideCount + " of " + slick.slideCount);
+//   } else {
+//     $('.controls').html(nrCurrentSlide + " - " + totalSlidesPerPage + " of " + slick.slideCount);
+//   }
 
-  console.log(current);
-  console.log(totalSlidesPerPage);
+//   console.log(current);
+//   console.log(totalSlidesPerPage);
 
-});
+// });
 
 
 
- $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    speed: 50,
-     cssEase: 'linear',
-    dots: true,
-    centerMode: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    arrows: false,
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true,
-    variableWidth: true,
-    //autoplay: true,
-});
+//  $('.slider-for').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     speed: 50,
+//      cssEase: 'linear',
+//     dots: true,
+//     centerMode: true,
+//     asNavFor: '.slider-nav'
+// });
+// $('.slider-nav').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-for',
+//     arrows: false,
+//     dots: false,
+//     centerMode: true,
+//     focusOnSelect: true,
+//     variableWidth: true,
+//     //autoplay: true,
+// });
 
-$('.slick-fade').slick({
-    dots: false,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2500,
-		infinite: true,
-		fade: true,
-		//speed: 100,
-		cssEase: 'linear'
-});
+// $('.slick-fade').slick({
+//     dots: false,
+// 		arrows: false,
+// 		autoplay: true,
+// 		autoplaySpeed: 2500,
+// 		infinite: true,
+// 		fade: true,
+// 		//speed: 100,
+// 		cssEase: 'linear'
+// });
 
 
 //config for animation on scroll
